7ac6558a155be9d06ef5efb30ff78a6ed06c2287
Aneela Majeed <51635775+Aneela-Majeed@users.noreply.github.com>
2026-05-29 11:17:49 -0700

    Initial commit - Add complete cakes bakery website
    Full responsive bakery website with home, about, gallery, and contact pages. Includes Bootstrap, CSS, JS, and images.

---
+++ new.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link href="css/login_overlay.css" rel='stylesheet' type='text/css' />
+
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+</head>
+<body>
+	<div class="container-fluid"></div>
+	<div class="row">
+		<div class="col-md-12" style="background-image: url(images/3.jpg);
+			background-size:100%;
+			background-repeat: no-repeat;width: 100%;height: 630px">
+			<div class="row">
+				<div class="col-md-8" ></div>
+				<div class="col-md-4">
+<div class="agile_its_registration bg-white">
+						<h3 class="title-2 mb-3">Order a Cake</h3>
+						<p >Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+						<form action="#" method="post">
+							<div class="agileits-location form-group">
+								<label>Shape</label>
+								<select required>
+									<option value="">Round</option>
+									<option value="1">rectangle</option>
+									<option value="2">square</option>
+									<option value="2">custom</option>
+								</select>
+								<div class="clear"></div>
+							</div>
+							<div class="agileits-location w3ls-1 form-group">
+								<label>Levels</label>
+								<select required>
+									<option value="">1</option>
+									<option value="1">2</option>
+									<option value="2">3</option>
+									<option value="2">4</option>
+								</select>
+								<div class="clear"></div>
+							</div>
+							<div class="agileits-location form-group">
+								<label>Size</label>
+								<select required>
+									<option value="">4 inch</option>
+									<option value="">6 inch</option>
+									<option value="">8 inch</option>
+									<option value="">10 inch</option>
+								</select>
+								<div class="clear"></div>
+							</div>
+							<div class="agileits-location w3ls-1 form-group">
+								<label>Flavor</label>
+								<select required>
+									<option value="">Chocolate</option>
+									<option value="">Butterscotch </option>
+									<option value="">Strawberry</option>
+									<option value="">Vanilla</option>
+								</select>
+								<div class="clear"></div>
+							</div>
+							<input type="submit" value="Order" class="agileinfo-order btn" />
+						</form>
+					</div>
+				</div>	
+			</div>		
+
+
+
+			
+		</div>
+		
+	</div>
+
+</body>
+</html>
